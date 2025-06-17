6fbd8dc890675f359e4e6e036be47d82211b5a4c
wangyiran_2 <241127072@fzu.edu.cn>
2025-06-17 09:28:58 +0800

    配置输入项目为spp

--- src/rnx2rtkp.c
+++ src/rnx2rtkp.c
@@ -191,12 +191,12 @@ int main(int argc, char **argv)
     ////infile[3] = "D:\\Desktop\\demo04\\test01\\WUM0MGXFIN_20192740000_01D_30S_CLK.CLK";
     //outfile = "D:\\Desktop\\demo04\\test01\\chan_source_cmpbds.pos";
     //3.04版本用混合系统包含北斗的进行测试
-    n = 4;
-    infile[0] = "d:\\desktop\\data2\\wuh20320.25o";
-    infile[1] = "d:\\desktop\\data2\\brdm0320.25p";
-    infile[2] = "d:\\desktop\\data2\\WUM0MGXFIN_20250320000_01D_05M_ORB.SP3";
-    infile[3] = "d:\\desktop\\data2\\WUM0MGXFIN_20250320000_01D_30S_CLK.CLK";
-    outfile = "d:\\desktop\\data2\\bds_result_2_B1I_B2a.pos";
+    //n = 4;
+    //infile[0] = "d:\\desktop\\data2\\wuh20320.25o";
+    //infile[1] = "d:\\desktop\\data2\\brdm0320.25p";
+    //infile[2] = "d:\\desktop\\data2\\WUM0MGXFIN_20250320000_01D_05M_ORB.SP3";
+    //infile[3] = "d:\\desktop\\data2\\WUM0MGXFIN_20250320000_01D_30S_CLK.CLK";
+    //outfile = "d:\\desktop\\data2\\bds_result_2_B1I_B2a.pos";
     //printf("opening file: [%s]\n", infile[0]);
     //printf("path length  : %zu\n", strlen(infile[0]));
     //用林提供的数据运行发现运行失败，只选中北斗无数据，加了gps发现解算的数据质量不好，中间丢失了大块的时间段
@@ -212,10 +212,10 @@ int main(int argc, char **argv)
 
 
     /*这里进行用北斗来spp的调试看看效果如何*/
-     	 //n = 2;
-       // infile[0] = "d:\\desktop\\spp_bds\\wuh20320.25o";
-       // infile[1] = "d:\\desktop\\spp_bds\\brdm0320.25p";
-       // outfile = "d:\\desktop\\spp_bds\\spp_bds_text.pos";
+     	 n = 2;
+        infile[0] = "d:\\desktop\\spp_bds\\wuh20320.25o";
+        infile[1] = "d:\\desktop\\spp_bds\\brdm0320.25p";
+        outfile = "d:\\desktop\\spp_bds\\spp_bds_text.pos";
 
     /*冰菓视频版本进行解算,执行失败，应该是有其他地方还需要更改的*/
     //n = 8;
